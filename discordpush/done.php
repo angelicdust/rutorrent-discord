0fae1a3fbd51241783a405759b78e2b6324025b9
samicrusader <hi@samicrusader.me>
2024-08-25 03:10:41 -0400

    chore: move discordpush/ dir into repo root

--- discordpush/done.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$req = new rXMLRPCRequest(array(
-    rTorrentSettings::get()->getOnInsertCommand(array('tdiscord'.User::getUser(), getCmd('cat='))),
-    rTorrentSettings::get()->getOnFinishedCommand(array('tdiscord'.User::getUser(), getCmd('cat='))),
-    rTorrentSettings::get()->getOnEraseCommand(array('tdiscord'.User::getUser(), getCmd('cat=')))
-));
-$req->run();
